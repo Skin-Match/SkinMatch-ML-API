85a4b72df6f5eef430ea5a1ac112cd9aa9e54b25
nabilmakarim11111 <93192525+nabilmakarim11111@users.noreply.github.com>
2024-06-18 18:21:03 +0700

    update

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
+import os
 from flask import Flask
 from compatibility import compatibility_blueprint
 from imageSave import image_save_blueprint
 from recSystem import rec_system_blueprint
+from recommendation import recommendations_blueprint
+
 
 app = Flask(__name__)
-app.register_blueprint(compatibility_blueprint, url_prefix='/compatibility')
-app.register_blueprint(image_save_blueprint, url_prefix='/image_save')
-app.register_blueprint(rec_system_blueprint, url_prefix='/rec_system')
+
+app.register_blueprint(compatibility_blueprint, url_prefix='')
+app.register_blueprint(image_save_blueprint, url_prefix='')
+app.register_blueprint(rec_system_blueprint, url_prefix='')
+app.register_blueprint(recommendations_blueprint, url_prefix='')
+
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # Set the port to the value of the PORT environment variable if available, or 8080 if not
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port, debug=True)
